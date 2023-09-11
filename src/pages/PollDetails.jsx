20cdb2399d3f076acb991c6483023a0e5d03aba2
kris.nguyen <kris.nguyen@avepoint.com>
2023-09-11 18:34:36 +0700

    update avatar

--- src/pages/PollDetails.jsx
+++ src/pages/PollDetails.jsx
@@ -4,26 +4,51 @@ import { doc, getDoc, arrayUnion, updateDoc } from "firebase/firestore";
 import { auth, db } from "../firebase";
 import { useDocument, useDocumentData } from "react-firebase-hooks/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { twMerge } from "tailwind-merge";
 
-const PollOptionAfterVote = ({ width, text }) => {
+const PollOptionAfterVote = ({ width, text, loading, data, option }) => {
     const [currentWidth, setCurrentWidth] = useState(0);
+    const users = data.users;
 
     useEffect(() => {
         setTimeout(() => {
             setCurrentWidth(width);
         }, 200);
-    }, []);
+    }, [width, text]);
 
     return (
         <div className="border-2 border-accent px-4 py-1 pb-2 rounded-md">
             <div className="flex items-center mb-2 justify-between">
-                <span className="text-[#666] font-semibold text-base">{text}</span>
+                <div className="/flex items-center gap-2">
+                    <span className="text-[#666] font-semibold text-base">{text}</span>
+                    <div className="flex items-center">
+                        {users
+                            .filter((user) => option.selectedUsers.includes(user.uid))
+                            .map((voted, index) => (
+                                <img
+                                    key={voted.uid}
+                                    className="w-6 rounded-full border-[1px] border-white"
+                                    style={{ marginLeft: -6 * index }}
+                                    src={voted.photoURL}
+                                />
+                            ))}
+                    </div>
+                </div>
                 <span>{`${Math.floor(currentWidth)}%`}</span>
             </div>
             <div key={text} className="bg-accent bg-opacity-20 h-2 rounded-md relative overflow-hidden">
                 <div className="absolute left-0 top-0 bottom-0 bg-accent transition-all duration-[500ms]" style={{ width: `${currentWidth}%` }}></div>
             </div>
         </div>
+    );
+};
+
+const PollOptionBeforeVote = ({ text, setSelectedOption }) => {
+    return (
+        <label htmlFor={text} className="border-accent border-2 rounded-md p-3 max-w-[300px] flex items-center gap-2 hover:opacity-75 cursor-pointer">
+            <input id={text} type="radio" value={text} name="option" onChange={(e) => setSelectedOption(e.target.value)} />
+            <span className="font-semibold text-base text-[#555]">{text}</span>
+        </label>
     );
 };
 
@@ -89,16 +114,24 @@ const PollDetails = () => {
         const isVoted = options.some((option) => option.selectedUsers.includes(user.uid));
         if (!isVoted) {
             return (
-                <div>
-                    <div>
+                <div className="w-[300px]">
+                    <div className="flex flex-col gap-2 mb-4">
                         {options.map((option) => (
-                            <div key={option.optionText}>
-                                <input type="radio" value={option.optionText} name="option" onChange={(e) => setSelectedOption(e.target.value)} />
-                                {option.optionText}
-                            </div>
+                            <PollOptionBeforeVote key={option.optionText} text={option.optionText} setSelectedOption={setSelectedOption} />
                         ))}
                     </div>
-                    <button onClick={handleVote}>Lưu lựa chọn</button>
+                    <div className="flex items-center justify-center">
+                        <button
+                            disabled={!selectedOption}
+                            className={twMerge(
+                                "text-white bg-accent py-2 px-3 text-sm rounded-md hover:opacity-75",
+                                !selectedOption && "opacity-20 hover:opacity-20"
+                            )}
+                            onClick={handleVote}
+                        >
+                            Lưu lựa chọn
+                        </button>
+                    </div>
                 </div>
             );
         }
@@ -106,7 +139,14 @@ const PollDetails = () => {
         return (
             <div className="flex flex-col gap-2 max-w-[300px]">
                 {options.map((option) => (
-                    <PollOptionAfterVote text={option.optionText} key={option.optionText} width={(option.votes / numberOfUsers) * 100} />
+                    <PollOptionAfterVote
+                        text={option.optionText}
+                        key={option.optionText}
+                        width={(option.votes / numberOfUsers) * 100}
+                        loading={loading}
+                        option={option}
+                        data={data}
+                    />
                 ))}
             </div>
         );
